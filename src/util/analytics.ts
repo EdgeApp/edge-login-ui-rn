6a8203bc1e47dfaefd0350bdd318576faf8291f1
Jon Tzeng <jon@edge.app>
2023-02-02 15:57:05 -0800

    Type tracking event names

--- src/util/analytics.ts
+++ src/util/analytics.ts
@@ -1,4 +1,14 @@
-export function logEvent(event: string) {
+export type TrackingEventName =
+  | 'Signup_Create_Account'
+  | 'Signup_Create_User_Success'
+  | 'Signup_Password_Valid'
+  | 'Signup_Review_Done'
+  | 'Signup_Terms_Agree_and_Create_User'
+  | 'Signup_Username_Available'
+  | 'Signup_Username_Unavailable'
+  | 'Signup_Welcome_Next'
+
+export function logEvent(event: TrackingEventName) {
   // @ts-expect-error
   const { firebase } = global
   if (firebase != null) firebase.analytics().logEvent(event)
