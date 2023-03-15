73f558c5989cc19c50776afcc7fbdf811467a186
Paul Puey <paul@edge.app>
2023-03-14 19:22:01 -0700

    Change new account review
    - Don't require showing acct creds to continue
    - Change wording to not require writing down password

--- src/components/scenes/newAccount/NewAccountReviewScene.tsx
+++ src/components/scenes/newAccount/NewAccountReviewScene.tsx
@@ -4,14 +4,12 @@ import { cacheStyles } from 'react-native-patina'
 
 import { confirmAndFinish } from '../../../actions/CreateAccountActions'
 import s from '../../../common/locales/strings'
-import { useScrollToEnd } from '../../../hooks/useScrollToEnd'
 import { Dispatch, RootState } from '../../../types/ReduxTypes'
 import { logEvent } from '../../../util/analytics'
 import { connect } from '../../services/ReduxStore'
 import { Theme, ThemeProps, withTheme } from '../../services/ThemeContext'
 import { AccountInfo } from '../../themed/AccountInfo'
 import { EdgeText } from '../../themed/EdgeText'
-import { Fade } from '../../themed/Fade'
 import { FormError } from '../../themed/FormError'
 import { MainButton } from '../../themed/MainButton'
 import { ThemedScene } from '../../themed/ThemedScene'
@@ -27,9 +25,6 @@ type Props = OwnProps & DispatchProps & ThemeProps
 const NewAccountReviewSceneComponent = ({ onDone, theme }: Props) => {
   const styles = getStyles(theme)
 
-  const [showNext, setShowNext] = React.useState(false)
-  const scrollViewRef = useScrollToEnd(showNext)
-
   const handleNext = () => {
     logEvent(`Signup_Review_Done`)
     onDone()
@@ -37,27 +32,25 @@ const NewAccountReviewSceneComponent = ({ onDone, theme }: Props) => {
 
   return (
     <ThemedScene title={s.strings.review}>
-      <ScrollView ref={scrollViewRef} contentContainerStyle={styles.content}>
+      <ScrollView contentContainerStyle={styles.content}>
         <EdgeText style={styles.description} numberOfLines={2}>
-          {s.strings.almost_done}
+          {s.strings.tap_to_review_button}
         </EdgeText>
         <FormError
           marginRem={[0, 0, 2]}
-          title={s.strings.write_and_store}
+          title={s.strings.alert_dropdown_warning}
           numberOfLines={2}
           isWarning
         >
           {s.strings.warning_message}
         </FormError>
-        <AccountInfo marginRem={[0, 2.5]} onOpen={() => setShowNext(true)} />
+        <AccountInfo marginRem={[0, 2.5]} />
         <View style={styles.actions}>
-          <Fade visible={showNext}>
-            <MainButton
-              label={s.strings.create}
-              type="secondary"
-              onPress={handleNext}
-            />
-          </Fade>
+          <MainButton
+            label={s.strings.create}
+            type="secondary"
+            onPress={handleNext}
+          />
         </View>
       </ScrollView>
     </ThemedScene>
