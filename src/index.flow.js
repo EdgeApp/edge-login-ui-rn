b890c103d0b9135a47b8f085a7fc9bd13ad54a1e
William Swanson <swansontec@gmail.com>
2024-02-28 15:40:13 -0800

    Update Flow types to better match reality
    We don't really use these or support these anymore, but make an effort to bring them closer.

--- src/index.flow.js
+++ src/index.flow.js
@@ -16,6 +16,10 @@ type ParentButton = {
   text: string
 }
 
+/**
+ * @deprecated This will not be provided in `fastLogin` mode
+ * Use `getSupportedBiometryType` and `touchIdEnabled` to learn these values.
+ */
 type TouchIdInfo = {
   isTouchSupported: boolean,
   isTouchEnabled: boolean
@@ -24,6 +28,10 @@ type TouchIdInfo = {
 type OnLogin = (account: EdgeAccount, touchIdInfo?: TouchIdInfo) => void
 
 type Unsubscribe = () => void
+
+// ---------------------------------------------------------------------
+// Components
+// ---------------------------------------------------------------------
 
 /**
  * Provides modals and other services for the login UI.
@@ -66,38 +74,124 @@ declare export class PasswordRecoveryScreen
 declare export class LoginScreen
   extends
     React.Component<{
+      // ---------------------------------------------------------------------
+      // Login functionality
+      // ---------------------------------------------------------------------
+
       context: EdgeContext,
 
-      // Branding stuff:
+      /** Options passed to the core login methods. */
+      accountOptions: EdgeAccountOptions,
+
+      /**
+       * Skip all post-login preparation steps.
+       *
+       * The goal is to display the main application as quickly as possible.
+       * Then, once the app is ready, several steps can happen in the background:
+       *
+       * - `prepareTouchId` to ensure the account can log in with biometrics.
+       * - `showNotificationPermissionReminder` to request permissions,
+       *   (or the app can provide its own reminder).
+       * - `showOtpReminder` to encourage the user to set up 2fa,
+       *   (or the app can provide its own reminder).
+       * - `watchSecurityAlerts` to check for incoming login requests,
+       *   followed by showing the `SecurityAlertsScreen` if detected.
+       *
+       * If you do not pass the `fastLogin` flag, edge-login-ui-rn itself
+       * will perform these steps, which can delay login by several seconds
+       * on slower phones.
+       */
+      fastLogin?: boolean,
+
+      /**
+       * The user to select, if present on the device.
+       * Get this from `EdgeContext.localUsers`
+       */
+      initialLoginId?: string,
+      // initialRoute?: InitialRouteName,
+
+      /** Pass a recoveryKey from the user's email to trigger recovery login. */
+      recoveryLogin?: string,
+
+      // ---------------------------------------------------------------------
+      // Branding & customization
+      // ---------------------------------------------------------------------
+
       appId?: string,
       appName?: string,
-      fontDescription?: { regularFontFamily: string },
       landingScreenText?: string,
       parentButton?: ParentButton,
       primaryLogo?: any,
       primaryLogoCallback?: () => void,
 
-      // Options passed to the core login methods:
-      accountOptions: EdgeAccountOptions,
+      /** Contains the terms of service URL */
+      // appConfig?: AppConfig,
 
-      // Called when the login completes:
+      /**  Behavior and appearance management flags, for A/B testing. */
+      // experimentConfig?: ExperimentConfig,
+
+      // ---------------------------------------------------------------------
+      // Callbacks
+      // ---------------------------------------------------------------------
+
+      /**
+       * Called when the user navigates back past the initialRoute if it was set.
+       */
+      onComplete?: () => void,
+
+      /*
+       * Called when the login completes.
+       */
       onLogin: OnLogin,
 
-      // The recoveryKey from the user's email, to trigger recovery login:
-      recoveryLogin?: string,
+      /**
+       * Records events for analytics.
+       */
+      // onLogEvent?: OnLogEvent,
 
+      // ---------------------------------------------------------------------
+      // Deprecated
+      // ---------------------------------------------------------------------
+
+      /**
+       * Called at login.
+       *
+       * @deprecated Pass `fastLogin` instead, and then perform
+       * your custom permissions logic after receiving `onLogin`.
+       */
+      customPermissionsFunction?: () => void,
+
+      /**
+       * @deprecated Use the LoginUiProvider component for theming.
+       */
+      fontDescription?: { regularFontFamily: string },
+
+      /**
+       * Called when the user makes a choice from RequestPermissionsModal.
+       * @deprecated Use `fastLogin` mode instead.
+       */
+      onNotificationPermit?: (settings: any) => void,
+
+      /**
+       * Do not show the OTP reminder during login.
+       *
+       * @deprecated Pass `fastLogin` instead
+       */
       skipOtpReminder?: boolean,
 
-      // Do not show the security alerts screen during login,
-      // since the app plans to show the `SecurityAlertsScreen` itself
-      // based on `hasSecurityAlerts` and `watchSecurityAlerts`:
+      /**
+       * Do not show the security alerts screen during login.
+       *
+       * @deprecated Pass `fastLogin` instead.
+       */
       skipSecurityAlerts?: boolean,
 
-      // The username to select, if present on the device:
-      username?: string,
-
-      // Call that overwrites the internal checkAndRequestNotifications function. Executed on Login initialization:
-      customPermissionsFunction?: () => void
+      /**
+       * The username to select, if present on the device.
+       *
+       * @deprecated Use initialLoginId instead.
+       */
+      username?: string
     }> {}
 
 declare export class OtpRepairScreen
@@ -117,10 +211,29 @@ declare export class SecurityAlertsScreen
       onComplete: () => void
     }> {}
 
-declare export function isTouchEnabled(account: EdgeAccount): Promise<boolean>
-declare export function enableTouchId(account: EdgeAccount): Promise<void>
-declare export function disableTouchId(account: EdgeAccount): Promise<void>
-declare export function getSupportedBiometryType(): Promise<BiometryType | null>
+// ---------------------------------------------------------------------
+// Post-login steps
+// ---------------------------------------------------------------------
+
+/**
+ * Should be called at login to ensure biometric login is properly enabled.
+ * @param account The account that has just logged in.
+ */
+declare export function refreshTouchId(account: EdgeAccount): Promise<void>
+
+/**
+ * Asks the users for notification permissions, if needed.
+ */
+declare export function showNotificationPermissionReminder(opts: {
+  appName?: string,
+  // onLogEvent?: OnLogEvent,
+  onNotificationPermit?: (settings: any) => void
+}): Promise<void>
+
+/**
+ * Check and show the 2fa reminder on login, if necessary.
+ */
+declare export function showOtpReminder(account: EdgeAccount): Promise<void>
 
 /**
  * Returns true if the application should show the SecurityAlertsScreen.
@@ -134,3 +247,12 @@ declare export function watchSecurityAlerts(
   account: EdgeAccount,
   onChange: (hasAlerts: boolean) => void
 ): Unsubscribe
+
+// ---------------------------------------------------------------------
+// Biometric utilities
+// ---------------------------------------------------------------------
+
+declare export function disableTouchId(account: EdgeAccount): Promise<void>
+declare export function enableTouchId(account: EdgeAccount): Promise<void>
+declare export function getSupportedBiometryType(): Promise<BiometryType | null>
+declare export function isTouchEnabled(account: EdgeAccount): Promise<boolean>
