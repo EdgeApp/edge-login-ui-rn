22c609ecacaabcc47429db5ae5beab3227304d4d
William Swanson <swansontec@gmail.com>
2024-09-03 17:03:28 -0700

    Remove unused redux actions

--- src/types/ReduxActions.ts
+++ src/types/ReduxActions.ts
@@ -1,5 +1,3 @@
-import { EdgeAccount } from 'edge-core-js'
-
 import { SceneState } from '../reducers/SceneReducer'
 import { TouchState } from '../reducers/TouchReducer'
 
@@ -7,7 +5,5 @@ export type Action =
   // Actions with no payload:
   | { type: 'RESET_APP' }
   // Actions with known payloads:
-  | { type: 'AUTH_UPDATE_PIN'; data: string }
-  | { type: 'CREATE_ACCOUNT_SUCCESS'; data: EdgeAccount }
   | { type: 'NAVIGATE'; data: SceneState }
   | { type: 'SET_TOUCH'; data: TouchState }
