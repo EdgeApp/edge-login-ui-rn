e4cd7fedc6ca3f5e7dc1c8ab189446b36cdf1849
Sam Holmes <sam@edge.app>
2024-10-31 12:15:40 -0700

    Change invalid password message semantic to invalid credentials

--- src/components/scenes/PasswordLoginScene.tsx
+++ src/components/scenes/PasswordLoginScene.tsx
@@ -235,7 +235,7 @@ export const PasswordLoginScene = (props: Props) => {
               )
             )
           } else {
-            setPasswordErrorMessage(lstrings.invalid_password)
+            setPasswordErrorMessage(lstrings.invalid_credentials)
           }
           return
         }
